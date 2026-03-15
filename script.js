318073672329299036c23ac9966a59eec53bb909
Marius571 <marius.schrage@t-online.de>
2026-03-15 18:09:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+function showMessage() {
+  const message = document.getElementById("message");
+  message.textContent =
+    "Der Prototyp zeigt eine erste Struktur für eine generationenübergreifende Vereinsplattform.";
+}
